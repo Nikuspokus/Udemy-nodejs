ac2c05d7eebbf334796290ab046e650c22964b4b
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-11 10:26:45 +0200

    Chap 5 - le systeme des modules - module HTTP - Fini

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 require('babel-register');
-const fs = require('fs');
+const http = require('http');
 
-fs.readFile('test.txt', 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err)
+http.createServer((req, res) => {
+
+  if (req.url =='/') {
+    res.writeHead(200, {'Content-type' : 'text/html'})
+    res.write("<h1>Accueil\n</h1>")
   } else {
-    console.log(data)
-
-    fs.writeFile('test.txt', 'Hello World !', 'utf-8', (err) => {
-      fs.readFile('test.txt', 'utf-8', (err, data) => {
-        console.log(data);
-      })
-    })
+    res.writeHead(404, {'Content-type' : 'text/html'})
+    res.write("<span style='color:red'>Erreur 404</span>")
   }
-})
+  res.end()
+}).listen(8080)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////
+//  Module FS
+////////////////////////////////////////
+// const fs = require('fs');
+
+// fs.readFile('test.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(data)
+
+//     fs.writeFile('test.txt', 'Hello World !', 'utf-8', (err) => {
+//       fs.readFile('test.txt', 'utf-8', (err, data) => {
+//         console.log(data);
+//       })
+//     })
+//   }
+// })
 
 
 ////////////////////////////////////////
