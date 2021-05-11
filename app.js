e98e43230032e37c943887105faf76682ea81863
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-11 09:46:45 +0200

    Chap 5 - le systeme des modules - WIP

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-require("babel-register");
+require('babel-register');
 
-function say(word) {
-  console.log(word);
-}
 
-function execute(someFunction, value) {
-  someFunction(value);
-}
 
-execute(say, "test");
 
-setTimeout(function execute() {
-    console.log('test 2');
-}, 1000)
 
-setTimeout(() => console.log('test fonction simplifier'))
+////////////////////////////////////////
+//  Module OS
+////////////////////////////////////////
+
+// const os = require('os');
+
+// console.log(os.arch());
+// console.log(os.homedir());
+
+
+////////////////////////////////////////
+//  Utilisation ECMAScript6 (ES6)
+////////////////////////////////////////
+
+// require("babel-register");
+
+// function say(word) {
+//   console.log(word);
+// }
+
+// function execute(someFunction, value) {
+//   someFunction(value);
+// }
+
+// execute(say, "test");
+
+// setTimeout(function execute() {
+//     console.log('test 2');
+// }, 1000)
+
+// setTimeout(() => console.log('test fonction simplifier'))
