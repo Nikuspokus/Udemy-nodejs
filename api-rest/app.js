64859374ed643f50ae4d2c39edf1bf56d6cc4f37
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-11 17:31:22 +0200

    Chap 46 - parametres des urls - WIP

---
+++ api-rest/app.js
@@ -0,0 +1,14 @@
+require('babel-register')
+
+const express = require('express')
+const app = express()
+
+app.get('/api',(req, res) => {
+    res.send('Root API')
+})
+
+app.get('/api/v1',(req, res) => {
+    res.send('API Version 1')
+})
+
+app.listen(8080, () => console.log('Started on port 8080'))
