7033505f1612094b68ab3291139effe0025622b8
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-11 10:02:45 +0200

    Chap 5 - le systeme des modules - module FS - Fini

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 require('babel-register');
+const fs = require('fs');
+
+fs.readFile('test.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data)
+
+    fs.writeFile('test.txt', 'Hello World !', 'utf-8', (err) => {
+      fs.readFile('test.txt', 'utf-8', (err, data) => {
+        console.log(data);
+      })
+    })
+  }
+})
 
 
 
