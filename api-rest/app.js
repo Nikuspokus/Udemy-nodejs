fc53ee9798213a195795d1410a8c9ea2539a6822
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-14 09:57:18 +0200

    Chap 52 - Gerer les parametres post - Fini

--- api-rest/app.js
+++ api-rest/app.js
@@ -1,55 +1,75 @@
-require('babel-register')
-const func = require('functions')
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
+require("babel-register");
+const func = require("functions");
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const app = express();
 
-const members =  [
-    {
-        id: 1,
-        name: 'John'
-    },
-    {
-        id: 2,
-        name: 'Julie'
-    },
-    {
-        id: 3,
-        name: 'Jack'
-    },
-]
+const members = [
+  {
+    id: 1,
+    name: "John",
+  },
+  {
+    id: 2,
+    name: "Julie",
+  },
+  {
+    id: 3,
+    name: "Jack",
+  },
+];
 ///////////////////////////////////////////////////////////
 //middleware morgan, permet d'avoir le temps de réponse de chaque requete
 ///////////////////////////////////////////////////////////
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(express.bodyParser.json())
 // app.use(express.bodyParser.urlencoded({ extended: true }))
 
-app.get('/api/v1/members/:id', (req, res) => {
-    res.json(func.success(members[(req.params.id)-1].name))
-})
+app.get("/api/v1/members/:id", (req, res) => {
+  res.json(func.success(members[req.params.id - 1].name));
+});
 
-app.get('/api/v1/members/', (req, res) => {
-    if (req.query.max != undefined && req.query.max > 0) {
-        res.json(func.success(members.slice(0, req.query.max)))
-    } else if (req.query.max != undefined) {
-        res.json(func.error('wrong max value'))
-    } else {
-        res.json(func.success(members))
+app.get("/api/v1/members/", (req, res) => {
+  if (req.query.max != undefined && req.query.max > 0) {
+    res.json(func.success(members.slice(0, req.query.max)));
+  } else if (req.query.max != undefined) {
+    res.json(func.error("wrong max value"));
+  } else {
+    res.json(func.success(members));
+  }
+});
+
+app.post("/api/v1/members", (req, res) => {
+  if (req.body.name) {
+    let sameName = false;
+    for (let i = 0; i < members.length; i++) {
+      if (members[i].name == req.body.name) {
+        sameName = true;
+        break;
+      }
     }
-})
 
-app.post('/api/v1/members', (req, res) =>{
-    res.send(req.body)
-})
+    if (sameName) {
+      res.json(func.error("name already used !!!"));
+    } else {
+      let member = {
+        id: members.length + 1,
+        name: req.body.name,
+      };
 
+      members.push(member);
 
-
+      res.json(func.success(member));
+    }
+  } else {
+    res.json(func.error("no name value"));
+  }
+});
 
 //programme qui s'execute avant tout le reste
 ///////////////////////////////////////////////////////////
@@ -76,5 +96,4 @@ app.post('/api/v1/members', (req, res) =>{
 //     res.send(req.params)
 // })
 
-app.listen(8080, () => console.log('Started on port 8080'))
-
+app.listen(8080, () => console.log("Started on port 8080"));
