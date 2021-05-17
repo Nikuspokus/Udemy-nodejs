460aca4f6fa641afadff4d734285a7bc7a2f0f16
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-17 17:41:57 +0200

    Chap 64 - Promesses en parallèle

--- api-rest/async/app.js
+++ api-rest/async/app.js
@@ -1,30 +1,49 @@
 require("babel-register");
 
 console.log("Début");
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("p1");
+  }, 1500);
+});
+let p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("p2");
+  }, 3000);
+});
 
-getMember()
-    .then(member => getArticles(member))
-    .then(articles => console.log(articles))
-    .catch(err => console.log(err.message))
+// promise .all ne s'affiche que lorsque les setTimeOut sont finis
 
-function getMember() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('Member 1');
-            resolve('Member 1')
-            // reject(new Error('Error during getMember()'))
-        }, 1500)
-    })
-}
+// le .all récupère tous les résultats
+Promise.all([p1, p2]).then((result) => console.log(result));
 
-function getArticles(member) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            // resolve([1,2,3])
-            reject(new Error('Error during getArticles()'))
-        }, 1500)
-    })
-}
+// le .race récupère le premier résultat qui arrive
+Promise.race([p1, p2]).then((result) => console.log(result));
+
+console.log("Fin");
+// getMember()
+//     .then(member => getArticles(member))
+//     .then(articles => console.log(articles))
+//     .catch(err => console.log(err.message))
+
+// function getMember() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('Member 1');
+//             resolve('Member 1')
+//             // reject(new Error('Error during getMember()'))
+//         }, 1500)
+//     })
+// }
+
+// function getArticles(member) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             // resolve([1,2,3])
+//             reject(new Error('Error during getArticles()'))
+//         }, 1500)
+//     })
+// }
 
 // new Promise((resolve, reject) => {
 //   setTimeout(() => {
@@ -34,9 +53,6 @@ function getArticles(member) {
 // })
 //   .then((message) => console.log(message))
 //   .catch((err) => console.log(err.message));
-console.log("Fin");
-
-
 
 // Callbacks
 // Promise
