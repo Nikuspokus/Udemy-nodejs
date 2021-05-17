137c847e53e20a5a3a5095c2104dafeed9408bab
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-17 18:14:41 +0200

    Chap 65 - Async & Await

--- api-rest/async/app.js
+++ api-rest/async/app.js
@@ -1,49 +1,73 @@
-require("babel-register");
+//////////////////////////////////////////////////////////////////
+//                          Les promesses                       //
+//////////////////////////////////////////////////////////////////
 
+// require("babel-register");
+
+// console.log("Début");
+// let p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("p1");
+//   }, 1500);
+// });
+// let p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("p2");
+//   }, 3000);
+// });
+
+// // promise .all ne s'affiche que lorsque les setTimeOut sont finis
+
+// // le .all récupère tous les résultats
+// Promise.all([p1, p2]).then((result) => console.log(result));
+
+// // le .race récupère le premier résultat qui arrive
+// Promise.race([p1, p2]).then((result) => console.log(result));
+
+// console.log("Fin");
+
+//////////////////////////////////////////////////////////////////
+//                          Async & Await                       //
+//////////////////////////////////////////////////////////////////
 console.log("Début");
-let p1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("p1");
-  }, 1500);
-});
-let p2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("p2");
-  }, 3000);
-});
 
-// promise .all ne s'affiche que lorsque les setTimeOut sont finis
-
-// le .all récupère tous les résultats
-Promise.all([p1, p2]).then((result) => console.log(result));
-
-// le .race récupère le premier résultat qui arrive
-Promise.race([p1, p2]).then((result) => console.log(result));
-
-console.log("Fin");
 // getMember()
 //     .then(member => getArticles(member))
 //     .then(articles => console.log(articles))
 //     .catch(err => console.log(err.message))
 
-// function getMember() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             console.log('Member 1');
-//             resolve('Member 1')
-//             // reject(new Error('Error during getMember()'))
-//         }, 1500)
-//     })
+(async () => {
+  try {
+    let member = await getMember();
+    let articles = await getArticles(member);
+    console.log(articles);
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
+
+// async function viewArticles() {
+
 // }
 
-// function getArticles(member) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             // resolve([1,2,3])
-//             reject(new Error('Error during getArticles()'))
-//         }, 1500)
-//     })
-// }
+function getMember() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Member 1");
+      // reject(new Error('Error during getMember()'))
+    }, 1500);
+  });
+}
+
+function getArticles(member) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([1, 2, 3]);
+      //   resolve(new Error("Error during getArticles()"));
+    }, 2000);
+  });
+}
+console.log("Fin");
 
 // new Promise((resolve, reject) => {
 //   setTimeout(() => {
