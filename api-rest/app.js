394ee0f7dd1424a37c5e8a47c42e49b842f4e229
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-17 12:13:26 +0200

    CREATION NEW BRANCH - Async sous nodejs

--- api-rest/app.js
+++ api-rest/app.js
@@ -1,9 +1,10 @@
 require("babel-register");
 const { success, error } = require("functions");
-const express = require("express");
-const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const config = require("./config.json");
 
 const members = [
   {
@@ -22,97 +23,102 @@ const members = [
 ///////////////////////////////////////////////////////////
 //middleware morgan, permet d'avoir le temps de réponse de chaque requete
 ///////////////////////////////////////////////////////////
+
+let MembersRouter = express.Router();
+
 app.use(morgan("dev"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(express.bodyParser.json())
-// app.use(express.bodyParser.urlencoded({ extended: true }))
+MembersRouter.route("/:id")
 
-app.get("/api/v1/members/:id", (req, res) => {
-  let index = getIndex(req.params.id);
+  //Récupère un membre avec son ID
+  .get((req, res) => {
+    let index = getIndex(req.params.id);
+    if (typeof index == "string") {
+      res.json(error(index));
+    } else {
+      res.json(success(members[index]));
+    }
+    res.json(success(members[req.params.id - 1].name));
+  })
 
-  if (typeof index == "string") {
-    res.json(error(index))
-  } else {
-    res.json(success(members[index]))
-  }
-  
-  res.json(success(members[req.params.id - 1].name));
-});
+  //Modifie un membre avec son ID
+  .put((req, res) => {
+    let index = getIndex(req.params.id);
 
-app.put("/api/v1/members/:id", (req, res) => {
-  let index = getIndex(req.params.id);
-  
-  if (typeof index == "string") {
-    res.json(error(index))
-  } else {
-    let same = false
-    
-    for (let i = 0; i < members.length; i++) {
-      if (req.body.name == members[i].name && req.params.id != members[i].id) {
-        same = true;
-        break
+    if (typeof index == "string") {
+      res.json(error(index));
+    } else {
+      let same = false;
+      for (let i = 0; i < members.length; i++) {
+        if (
+          req.body.name == members[i].name &&
+          req.params.id != members[i].id
+        ) {
+          same = true;
+          break;
+        }
+      }
+      if (same) {
+        res.json(error("same name !!"));
+      } else {
+        members[index].name = req.body.name;
+        res.json(success(true));
       }
     }
-    if (same) {
-      res.json(error("same name !!"))
-    } else {
-      members[index].name = req.body.name;
-      res.json(success(true))
-    }
-  }
-})
+  })
 
-app.delete('/api/v1/members/:id', (req, res) => {
-  let index = getIndex(req.params.id);
+  //Supprime un membre avec son ID
+  .delete((req, res) => {
+    let index = getIndex(req.params.id);
+    if (typeof index == "string") {
+      res.json(error(index));
+    } else {
+      members.splice(index, 1);
+      res.json(success(members));
+    }
+  });
+
+MembersRouter.route("/")
+
+  //Récupère tous les membres
+  .get((req, res) => {
+    if (req.query.max != undefined && req.query.max > 0) {
+      res.json(success(members.slice(0, req.query.max)));
+    } else if (req.query.max != undefined) {
+      res.json(error("wrong max value"));
+    } else {
+      res.json(success(members));
+    }
+  })
   
-  if (typeof index == "string") {
-    res.json(error(index))
-  } else {
-    members.splice(index, 1)
-    res.json(success(members))
-  }
-})
-
-app.get("/api/v1/members/", (req, res) => {
-  if (req.query.max != undefined && req.query.max > 0) {
-    res.json(success(members.slice(0, req.query.max)));
-  } else if (req.query.max != undefined) {
-    res.json(error("wrong max value"));
-  } else {
-    res.json(success(members));
-  }
-});
-
-app.post("/api/v1/members", (req, res) => {
-  if (req.body.name) {
-    let sameName = false;
-    for (let i = 0; i < members.length; i++) {
-      if (members[i].name == req.body.name) {
-        sameName = true;
-        break;
+  //Ajoute un membre
+  .post((req, res) => {
+    if (req.body.name) {
+      let sameName = false;
+      for (let i = 0; i < members.length; i++) {
+        if (members[i].name == req.body.name) {
+          sameName = true;
+          break;
+        }
       }
-    }
 
-    if (sameName) {
-      res.json(error("name already used !!!"));
+      if (sameName) {
+        res.json(error("name already used !!!"));
+      } else {
+        let member = {
+          id: createID(),
+          name: req.body.name,
+        };
+        members.push(member);
+        res.json(success(member));
+      }
     } else {
-      let member = {
-        id: createID(),
-        name: req.body.name,
-      };
-
-      members.push(member);
-
-      res.json(success(member));
+      res.json(error("no name value"));
     }
-  } else {
-    res.json(error("no name value"));
-  }
-})
-
+  });
 
 //programme qui s'execute avant tout le reste
 ///////////////////////////////////////////////////////////
@@ -139,7 +145,9 @@ app.post("/api/v1/members", (req, res) => {
 //     res.send(req.params)
 // })
 
-app.listen(8080, () => console.log("Started on port 8080"));
+app.use(config.return + "members", MembersRouter);
+
+app.listen(config.port, () => console.log("Started on port " + config.port));
 
 function getIndex(id) {
   for (let i = 0; i < members.length; i++) {
@@ -150,5 +158,5 @@ function getIndex(id) {
 }
 
 function createID() {
-  return members[members.length-1].id + 1
+  return members[members.length - 1].id + 1;
 }
